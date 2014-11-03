452e0699dfcc7fa345770f5d38c387b4b0cf1a4a
codehead <kbuimscd@gmail.com>
2014-11-02 22:11:14 -0700

    4th commit, working with server, json problem

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var http = require('http');
+var request = require('request');
 
 //adding database
 var mongo = require('mongodb');
@@ -13,13 +14,13 @@ var monk = require('monk');
 var mongo = require('mongodb');
 var mongostr= "mongodb://root:test@ds047940.mongolab.com:47940/heroku_app31160925";
 var db = monk(mongostr);
-var api = db.get('source');
+
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-var server = app.listen( process.env.PORT || 3000);
+var server = app.listen( process.env.PORT || 5000);
 var io = require('socket.io').listen(server);
 
 // view engine setup
@@ -74,10 +75,57 @@ app.use(function(err, req, res, next) {
     });
 });
 
-/*setInterval(function(){
-    var lastItem = api.find({},{last :{$slice:[n,n-1]}});
-io.sockets.emit('info',last);
+/*
+setInterval(function(){
+   // var lastItem = api.find({},{last :{$slice:[1,2]}});
+io.sockets.emit('info',"info to go to front end");
 },3000);
 */
+function scrape_btc_e(urlstr, collection){
+    setInterval(function(){
+    request(urlstr,function(e,json){
+    if(e) console.log("error with scraping");
+    console.log("Connecting..."+json+e);
+        var body = json.body;
+        var jbody=JSON.parse(body);
+        var btc_values=jbody.ltc_btc;   
+        var usd_values = jbody.ltc_usd; 
+        var newItem={"pair":"LTC-BTC","url":"https://btc-e.com/api/3/ticker/ltc_btc-ltc_usd","usd":0,"btc":btc_values.avg,"volume":btc_values.vol,"time":btc_values.last};
+        collection.insert(newItem); 
+        var newItem={"pair":"LTC-BTC","url":"https://btc-e.com/api/3/ticker/ltc_btc-ltc_usd","usd":usd_values.avg,"btc":0,"volume":usd_values.vol,"time":usd_values.last};  
+        collection.insert(newItem);
+    });
+    },3000);
+        
+}
+var urlstr= "https://btc-e.com/api/3/ticker/ltc_btc-ltc_usd";
+scrape_btc_e(urlstr,db.get('btc_edb'));
+
+
+
+function scrape_cryptsy(urlstr, collection){
+    setInterval(function(){
+    request(urlstr,function(e,json){
+    if(e) console.log("error with scraping");
+    console.log("Connecting...");
+        var body = json.body;
+        var jbody=JSON.parse(body);
+        var markets = jbody.return.markets;        
+        var btc_values=markets.LTC/BTC;  
+        var usd_values = jbody.LTC/USD;    
+        var newItem={"pair":"LTC-BTC","url":"https://btc-e.com/api/3/ticker/ltc_btc-ltc_usd","usd":0,"btc":btc_values.lasttradeprice,"volume":btc_values.volume,"time":btc_values.lasttradetime};
+        collection.insert(newItem); 
+        var newItem={"pair":"LTC-BTC","url":"https://btc-e.com/api/3/ticker/ltc_btc-ltc_usd","usd":usd_values.lasttradeprice,"btc":0,"volume":usd_values.volume,"time":usd_values.lasttradetime};   
+        collection.insert(newItem);
+        console.log(newItem);
+        
+        
+    });
+    },3000);
+        
+}
+var urlstr= "http://pubapi.cryptsy.com/api.php?method=marketdatav2";
+scrape_cryptsy(urlstr,db.get('cryptsydb'));
+
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,52 +1,40 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
-/*var app=require('app');
-var server = app.listen( process.env.PORT || 7000);
-var io = require('socket.io').listen(server);
-*/
 
-/* GET home page. 
+
+// GET home page. 
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('main', { title: 'Express' });
 });
-*/
 
-function scrape(urlstr, collection,req){
-	setInterval(function(){
-	request(urlstr,function(e,json){
-	if(e) console.log("error with scraping");
-	console.log("Connecting..."+json+e);
-		var body = json.body;
-		var jbody=JSON.parse(body);
-		var btc_usd=jbody.btc_usd;
-		console.log(btc_usd+" "+btc_usd.ave);
-		var newItem={"pair":"BTC-USD","url":"https://btc-e.com/api/3/ticker/btc_usd","usd":btc_usd.avg,"btc":1,"volume":btc_usd.vol,"time":btc_usd.last};
-		collection.insert(newItem);		
-		req.io.sockets.emit('info',newItem);	
-		
-	});
-	},30000);
-		
-}
 
-var db;
+
+
+
 /* Get Main page */
-router.get('/',function(req,res){
-	db = req.db;
-	var collection = db.get('source');		
+router.get('/btc-e.com',function(req,res){
+	var db = req.db;
+	var collection = db.get('btc_edb');		
 	collection.find({},function(e,docs){
-			res.render('main',{
-				"items" : docs
+			res.render('btc_e',{
+				"btc_e" : docs
 			});			
 		});	
-	//btc-e.com
-
-var urlstr= "https://btc-e.com/api/3/ticker/btc_usd";
-//setInterval(scrape(urlstr,collection),2000);
-scrape(urlstr,collection,req);
 	
 });
+
+/* Get info from cryptsy.com */
+router.get('/cryptsy',function(req,res){
+	db = req.db;
+	var collection = db.get('cryptsydb');		
+	collection.find({},function(e,docs){
+			res.render('cryptsy',{
+				"cryptsy" : docs
+			});			
+		});		
+});
+
 
 
 
